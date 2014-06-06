554694f02c8339c838752d5e2342461c4fefc6de
Reuzel <joost_reuzel@hotmail.com>
2014-06-06 13:11:10 +0200

    spec update

--- CqlSharp/BuildPackage.cmd
+++ CqlSharp/BuildPackage.cmd
@@ -1,9 +1,11 @@
 @echo off
-C:
-cd\
 
 IF EXIST "C:\NuGetRepository\%1\" GOTO build
+
+C:
+cd\
 mkdir NuGetRepository\%1\
+cd /d %~dp0
 
 :build
 nuget pack %~dp0\CqlSharp.csproj -Symbols -Prop Configuration=%1 -OutputDirectory c:\NuGetRepository\%1\
